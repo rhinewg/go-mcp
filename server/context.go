9a84e2a6342ddf847f586fbef443a417f4f80d1e
Lariel Fernandes <lariel.fernandes@dash0.com>
2025-05-17 16:14:27 +0200

    fix: remove func duplicate

--- server/context.go
+++ server/context.go
@@ -11,14 +11,10 @@ func setSessionIDToCtx(ctx context.Context, sessionID string) context.Context {
 	return context.WithValue(ctx, sessionIDKey{}, sessionID)
 }
 
-func getSessionIDFromCtx(ctx context.Context) (string, error) {
+func GetSessionIDFromCtx(ctx context.Context) (string, error) {
 	sessionID := ctx.Value(sessionIDKey{})
 	if sessionID == nil {
 		return "", errors.New("no session id found")
 	}
 	return sessionID.(string), nil
-}
-
-func GetSessionIDFromCtx(ctx context.Context) (string, error) {
-	return getSessionIDFromCtx(ctx)
 }
